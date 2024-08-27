d8982a9757c4f7e7028708f0ce73416beadc96e1
basicdev04 <tanishqkandpal.work@gmail.com>
2024-08-27 14:34:13 +0530

    reverted changes

--- frontend/src/components/UpdateBlog.tsx
+++ frontend/src/components/UpdateBlog.tsx
@@ -3,7 +3,7 @@ import { Appbar } from "../components/Appbar";
 import { useBlog } from "../hooks";
 import { Loader } from "../components/Loader";
 import axios from "axios";
-import { CreatePostType } from "../../../common/dist";
+import { CreatePostType } from "@basicdev04/common-app";
 import { useState, useEffect } from "react";
 import { useQuill } from "react-quilljs";
 
