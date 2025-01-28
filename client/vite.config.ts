1728b4001760720bddb236e0179f94ccabee465f
basicdev04 <tanishqkandpal.work@gmail.com>
2025-01-28 22:22:11 +0530

    editor issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from 'vite-plugin-commonjs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    commonjs() // Added to handle CommonJS modules
   ],
   optimizeDeps: {
-    include: ['react-quilljs', 'quill'], // Ensure these libraries are optimized by Vite
+    exclude: ['quill'], // Ensure these libraries are optimized by Vite
   },
   build: {
     commonjsOptions: {
-      include: [/react-quilljs/, /node_modules/], // Include react-quilljs in the commonjs handling
+      transformMixedEsModules:true// Include react-quilljs in the commonjs handling
     },
   },
 })
